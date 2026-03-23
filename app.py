e90a59248b612c0d4c850d223a54d234dd78b204
vactrajcev <vactrajcev@gmail.com>
2026-03-23 16:47:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 from datetime import datetime
 
 # Configurazione Pagina
-st.set_page_config(page_title="Gestione Turni Pro V32", layout="wide")
+st.set_page_config(page_title="Gestione Turni", layout="wide")
 st.title("🗓️ Sistema Gestione Turni - V32")
 st.markdown("### Vincoli, Assenze, Preferenze e Protezione Riposo")
 
